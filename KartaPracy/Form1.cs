4cc73374f971472f73db04969ff762c53cb9548d
Adam-Rem <80329664+Adam-Rem@users.noreply.github.com>
2022-12-12 16:27:14 +0100

    Add files via upload

---
+++ KartaPracy/Form1.cs
@@ -0,0 +1,21 @@
+using System.Drawing.Imaging;
+using System.Drawing;
+
+namespace KartaPracy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+        
+                
+    }
+}
